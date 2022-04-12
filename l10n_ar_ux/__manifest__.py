7949a051d0e12b8c1eb0c8af587deed532d17cd0
Angel Parra <aparra@aitic.com.ar>
2022-04-11 19:31:25 -0500

    Ticket 5294: Nombre en Recibo de Cobro.

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "13.0.1.19.0",
+    'version': "13.0.1.19.1",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
