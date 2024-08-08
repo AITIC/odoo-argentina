e85e39be960e62f39d18bd2234f771a6a0ceb41c
Guido Giustozzi <guido.giustozzi@gmail.com>
2024-08-08 15:48:22 -0300

    [FIX][l10n_ar_withholding_ux]Sacamos SQL_CONSTRAINTS para las retenciones IVA

--- l10n_ar_withholding_ux/models/l10n_ar_payment_withholding.py
+++ l10n_ar_withholding_ux/models/l10n_ar_payment_withholding.py
@@ -35,7 +35,8 @@ class l10nArPaymentRegisterWithholding(models.Model):
     previous_withholding_amount = fields.Monetary(readonly=True,)
     computed_withholding_amount = fields.Monetary(readonly=True,)
 
-    _sql_constraints = [('uniq_line', 'unique(tax_id, payment_id)', "El impuesto de retención debe ser único por pago")]
+    """CAMBIO REALIZADO POR GG. Sacamos unico impuesto en lineas de retencion"""
+    #_sql_constraints = [('uniq_line', 'unique(tax_id, payment_id)', "El impuesto de retención debe ser único por pago")]
 
     # def _compute_base_amount(self):
 
